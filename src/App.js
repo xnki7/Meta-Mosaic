8d4fecdd280333dfe0f8a623517a53015243b4a0
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-06-10 09:42:35 +0530

    packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import CreateNlistToken from "./pages/CreateNlistToken";
 
 function App() {
   return (
     <div className="App">
-     
+      <CreateNlistToken />
     </div>
   );
 }
---
+++ src/pages/CreateNlistToken.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateNlistToken() {
+  return <div className="CreateNlistToken">CreateNlistToken</div>;
+}
+
+export default CreateNlistToken;
